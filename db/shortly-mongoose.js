70be76e20c16f14d77583a22124be38aa6cf542f
Hack Reactor Students <accounts@hackreactor.com>
2017-03-09 17:16:56 -0800

    Refactored user and link to use mongoose. Changed saveLink to adopt the mongoose syntax.

--- db/shortly-mongoose.js
+++ db/shortly-mongoose.js
@@ -1,88 +1,16 @@
 var mongoose = require('mongoose');
-var autoIncrement = require('mongoose-auto-increment');
+// var autoIncrement = require('mongoose-auto-increment');
 
+mongoose.connect('mongodb://localhost/shortlydb');
 
-
-// mongoose.connect('mongodb://localhost/test');
-
-var connection = mongoose.createConnection('mongodb://localhost/test');
+// var connection = mongoose.createConnection('mongodb://localhost/test');
 
 var db = mongoose.connection;
-autoIncrement.initialize(db);
+// autoIncrement.initialize(db);
+
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', function() {
-  // we're connected!
-
-  // var kittySchema = mongoose.Schema({
-  //   name: String
-  // });
-
-  // var Kitten = mongoose.model('Kitten', kittySchema);
-
-
-
-
-
-  // var userSchema = new mongoose.Schema({
-  //   // id: { type: mongoose.Schema.Types.ObjectId},
-  //   username: String,
-  //   password: String,
-  //   timestamp: Date
-  // });
-
-  // userSchema.plugin(autoIncrement.plugin, 'User');
-
-  // // creating the class
-  // var User = mongoose.model('User', userSchema);
-
-  // module.exports.user = mongoose.model('User', userSchema);
-
-
-  // var linkSchema = new mongoose.Schema({
-  //   url: String,
-  //   baseUrl: String,
-  //   code: String,
-  //   title: String,
-  //   visits: Number,
-  //   timestamp: Date
-  // });
-
-  // linkSchema.plugin(autoIncrement.plugin, 'Link');
-
-  // var Link = mongoose.model('Link', linkSchema);
-
-  // module.exports.link = mongoose.model('Link', linkSchema);
-
-
-
-
-  // var brian = new User({username: 'brian', password: 'pass', timestamp: new Date()});
-
-
+  console.log('we\'re connected!');
 });
 
-
-var userSchema = new mongoose.Schema({
-  username: String,
-  password: String,
-  timestamp: Date
-});
-userSchema.plugin(autoIncrement.plugin, 'User');
-module.exports.user = mongoose.model('User', userSchema);
-
-var linkSchema = new mongoose.Schema({
-  url: String,
-  baseUrl: String,
-  code: String,
-  title: String,
-  visits: Number,
-  timestamp: Date
-});
-
-linkSchema.plugin(autoIncrement.plugin, 'Link');
-
-module.exports.link = mongoose.model('Link', linkSchema);
-
-
-
-
+module.exports = db;
